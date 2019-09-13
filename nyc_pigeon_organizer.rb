8ae57e76162e70288af45362640edfe2c156b693
Gaida M Escobar <gescobar129@gmail.com>
2019-09-13 03:56:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -15,7 +15,7 @@ def nyc_pigeon_organizer(data)
         value.each do |inner_key, inner_value|
           inner_value.each do |element|
             
-            if data[inner_key].
+            if data[inner_key].includes
               sub_hash[key] = sub_hash[key].push(inner_key)
             end
           puts sub_hash
