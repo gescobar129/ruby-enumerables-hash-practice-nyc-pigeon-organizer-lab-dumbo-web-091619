439e9926fd34ae587fb1417c32535c040a5685bb
Gaida M Escobar <gescobar129@gmail.com>
2019-09-13 04:39:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,8 +11,8 @@ def nyc_pigeon_organizer(data)
       name_hash = Hash[new_array.collect {|n| [n, hash = {}] } ]
       data.each do |attributes, options|
         
-        sub_hash = {}
-        sub_hash[attributes] = [];
+        attribute_hash = {}
+        attribute_hash[attributes] = [];
         
         options.each do |option_name, name_array|
           name_array.each do |name|
@@ -20,19 +20,11 @@ def nyc_pigeon_organizer(data)
 
             puts name_hash.keys[name]
             if name_hash.keys.include?(name)
-              sub_hash[attributes] = sub_hash[attributes].push(option_name)
+              attribute_hash[attributes] = attribute_hash[attributes].push(option_name)
             end
-          puts sub_hash
-          puts "OPOPOPOPOPOPOPOPPPPPOPOOPPOP"
           end
         end
       end
-      #new_array.each do |n|
-        #data.each do |key, value|
-         #new_hash = new_hash + n[key]
-      print new_hash
-    #print new_array
-    #puts "!!!!!!!!!!!!!!!!!!"
   
 end
 
