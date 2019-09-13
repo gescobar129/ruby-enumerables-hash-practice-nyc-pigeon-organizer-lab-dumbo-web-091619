faf40958b3a58914a8fdac7332f94354c4670740
Gaida M Escobar <gescobar129@gmail.com>
2019-09-13 03:34:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,6 +8,9 @@ def nyc_pigeon_organizer(data)
   #new_hash = {}
       new_array = new_array.uniq
       new_hash = Hash[new_array.collect {|n| [n, hash = {}] } ]
+      data.each do |key, value|
+        value.each do |inner_key, inner_value|
+          
       #new_array.each do |n|
         #data.each do |key, value|
          #new_hash = new_hash + n[key]
