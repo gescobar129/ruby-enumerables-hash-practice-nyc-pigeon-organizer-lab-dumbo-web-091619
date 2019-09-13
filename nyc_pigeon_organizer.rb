e7d409afa45c5c09295153180e8e0157b2482c96
Gaida M Escobar <gescobar129@gmail.com>
2019-09-13 01:44:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_array = []
+  data.each do |key, value|
+    value.each do |inner_key, inner_value|
+      new_array = new_array.concat(inner_value)
+    end
+  end
+  new_hash = {}
+      new_array = new_array.uniq
+      new_array.each do |n|
+        data.each do |key, value|
+          new_hash = n[key]
+      end
+    end
+      print new_hash
+    #print new_array
+    #puts "!!!!!!!!!!!!!!!!!!"
+  
 end
