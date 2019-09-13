23acd702f8f158c293233112ae3183f811f46634
Gaida M Escobar <gescobar129@gmail.com>
2019-09-13 05:36:37 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,31 @@
+require 'pry' 
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_array = []
+  data.each do |attributes, options|
+    options.each do |option_name, name_array|
+      new_array = new_array.concat(name_array)
+    end
+  end
+      new_array = new_array.uniq
+      name_hash = Hash[new_array.collect {|n| [n, hash = {}] } ]
+      
+      name_hash.each do |pigeon_name, empty_hash|
+        data.keys.each do |key|
+          empty_hash[key] = []
+        end
+        name_hash[pigeon_name] = empty_hash
+      end
+
+      data.each do |attributes, options|
+        options.each do |option_name, name_array|
+          name_array.each do |name|
+            
+            name_hash[name][attributes].push(option_name.to_s)
+            
+          end
+        end
+      end
+  return name_hash
 end
+
